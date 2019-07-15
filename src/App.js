3ac38d727ccc8a4fdb0a95c73f591bdcb1d80b16
Osama Adam <Osamaadam98@gmail.com>
2019-07-15 23:06:46 +0200

    Minor detail

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import './App.css'
 import Header from "./components/Header"
 import Main from "./components/Main"
+import Footer from "./components/Footer"
 
 
 class App extends Component {
@@ -10,6 +11,7 @@ class App extends Component {
       <div className="App">
         <Header/>
         <Main/>
+        <Footer/>
       </div>
     )
   }
