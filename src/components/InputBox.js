47f3509ef2f0c51f28f44e7ec1a0a817156844b7
Osama Adam <osamaadam98@gmail.com>
2019-07-25 22:42:14 +0200

    Converted InputBox into a functional component using hooks!

--- src/components/InputBox.js
+++ src/components/InputBox.js
@@ -1,49 +1,33 @@
-import React, {Component} from "react";
+import React, {useState} from "react";
+function InputBox(props) {
+	const [entry, setEntry] = useState("");
+	const [id, setID] = useState(props.todos.length);
 
-class InputBox extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			entry: "",
-			id: this.props.todos.length
-		};
-
-		this.handleChange = this.handleChange.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
-	}
-
-	render() {
-		return (
-			<div>
-				<form onSubmit={this.handleSubmit}>
-					<input
-						className="form-control inputBox"
-						type="text"
-						name="entry"
-						placeholder="add a new entry.."
-						onChange={this.handleChange}
-						autoFocus={true}
-						autoComplete={false}
-					/>
-				</form>
-			</div>
-		);
-	}
-
-	handleChange(event) {
-		const {name, value} = event.target;
-		this.setState({
-			[name]: value
-		});
-	}
-
-	handleSubmit(event) {
-		this.setState({
-			id: this.state.id + 1
-		});
-		this.props.newEntry(this.state.entry, this.state.id);
+	const handleSubmit = event => {
+		setID(id + 1);
+		props.newEntry(entry, id);
 		event.preventDefault();
-	}
+	};
+	const handleChange = event => {
+		setEntry(event.target.value);
+	};
+
+	return (
+		<div>
+			<form onSubmit={handleSubmit}>
+				<input
+					className="form-control inputBox"
+					type="text"
+					name="entry"
+					value={entry}
+					placeholder="add a new entry.."
+					onChange={handleChange}
+					autoFocus={true}
+					autoComplete="false"
+				/>
+			</form>
+		</div>
+	);
 }
 
 export default InputBox;
