7cc8bc42a4142cc778e33cdafc0cec5da99d76ae
Osama Adam <osamaadam98@gmail.com>
2019-07-25 23:27:56 +0200

    Turned Main into a functional component using hooks

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,51 +1,39 @@
-import React, {Component} from "react";
+import React, {useState} from "react";
 import CheckBox from "./CheckBox";
 import todosData from "./todosData";
 import InputBox from "./InputBox";
 
-class Main extends Component {
-	constructor() {
-		super();
-		this.handleChange = this.handleChange.bind(this);
-		this.newEntry = this.newEntry.bind(this);
-		this.state = {
-			todos: todosData
-		};
-	}
+function Main() {
+	const [todos, setTodos] = useState(todosData);
 
-	render() {
-		const checkBoxComponent = this.state.todos.map(item => (
-			<CheckBox key={item.id} item={item} handleChange={this.handleChange} />
-		));
-
-		return (
-			<div className="container-fluid">
-				<InputBox newEntry={this.newEntry} todos={this.state.todos} />
-				{checkBoxComponent}
-			</div>
-		);
-	}
-
-	handleChange(id) {
-		this.setState(prevState => {
-			const updatedState = prevState.todos.map(todo => {
+	const handleChange = (id) => {
+		setTodos(
+			todos.map((todo) => {
 				if (todo.id === id) todo.completed = !todo.completed;
 				return todo;
-			});
-			return {todos: updatedState};
-		});
-	}
+			})
+		);
+	};
 
-	newEntry(textEntry, itemId) {
-		this.setState(prevState => {
-			const updatedState = prevState.todos.concat({
-				id: itemId,
+	const newEntry = (textEntry, itemID) => {
+		setTodos(
+			todos.concat({
+				id: itemID,
 				entry: textEntry,
 				completed: false
-			});
-			return {todos: updatedState};
-		});
-	}
-}
+			})
+		);
+	};
 
+	const checkBoxComponent = todos.map((item) => (
+		<CheckBox key={item.id} item={item} handleChange={handleChange} />
+	));
+
+	return (
+		<div className="container-fluid">
+			<InputBox newEntry={newEntry} todos={todos} />
+			{checkBoxComponent}
+		</div>
+	);
+}
 export default Main;
