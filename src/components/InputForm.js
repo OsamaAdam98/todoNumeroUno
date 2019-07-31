41c7f4a4b8e1aad7a51762a53d464618e169f3ce
Osama Adam <osamaadam98@gmail.com>
2019-08-01 01:30:08 +0200

    Added client-side routing and changed the names of some components to be more relevant. Also fixed the NavBar and input box.

---
+++ src/components/InputForm.js
@@ -0,0 +1,40 @@
+import React, {useState} from "react";
+function InputForm(props) {
+	const [entry, setEntry] = useState("");
+
+	const handleSubmit = (event) => {
+		if (entry.trim()) props.newEntry(entry);
+		event.preventDefault();
+	};
+	const handleChange = (event) => {
+		setEntry(event.target.value);
+	};
+
+	return (
+		<div>
+			<form onSubmit={handleSubmit}>
+				<div className="input-group mb-3">
+					<input
+						className="form-control"
+						type="text"
+						name="entry"
+						value={entry}
+						placeholder="add a new entry.."
+						onChange={handleChange}
+						autoFocus={true}
+						autoComplete="false"
+					/>
+					<button
+						type="button"
+						className="input-group-append btn btn-danger"
+						onClick={props.flush}
+					>
+						Flush
+					</button>
+				</div>
+			</form>
+		</div>
+	);
+}
+
+export default InputForm;
---
+++ src/components/NavBar.js
@@ -0,0 +1,43 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+function NavBar() {
+	const currentPath = window.location.href;
+	const hostPath = window.location.origin;
+	return (
+		<div>
+			<nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
+				<Link to="/" className="navbar-brand">
+					todoList
+				</Link>
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-toggle="collapse"
+					data-target="#navbarNav"
+					aria-controls="navbarNav"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+				>
+					<span className="navbar-toggler-icon" />
+				</button>
+				<div className="collapse navbar-collapse" id="navbarNavDropdown">
+					<ul className="navbar-nav">
+						<li className="nav-item">
+							<Link to="/" className="nav-link">
+								Home <span class="sr-only" />
+							</Link>
+						</li>
+						<li className="nav-item">
+							<Link to="/about/" className="nav-link">
+								About
+							</Link>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</div>
+	);
+}
+
+export default NavBar;
