7f9c2afb0619ff6dfa238e04535dd9c0957682e9
Osama Adam <osamaadam98@gmail.com>
2019-08-01 02:44:56 +0200

    Fixed the hamburger menu on small screens.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,29 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import {Link} from "react-router-dom";
 
 function NavBar() {
+	const [navCollapsed, setNavCollapsed] = useState(true);
+
+	const toggleNav = () => {
+		setNavCollapsed(!navCollapsed);
+	};
+
 	return (
 		<div>
-			<nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
+			<nav className="navbar navbar-expand-lg navbar-light bg-light mb-3">
 				<Link to="/" className="navbar-brand">
 					todoList
 				</Link>
 				<button
-					className="navbar-toggler"
-					type="button"
-					data-toggle="collapse"
-					data-target="#navbarNav"
-					aria-controls="navbarNav"
 					aria-expanded="false"
-					aria-label="Toggle navigation"
+					className="navbar-toggle collapsed"
+					onClick={toggleNav}
+					type="button"
 				>
 					<span className="navbar-toggler-icon" />
 				</button>
-				<div className="collapse navbar-collapse" id="navbarNavDropdown">
+				<div className={(navCollapsed ? "collapse" : "") + " navbar-collapse"}>
 					<ul className="navbar-nav">
 						<li className="nav-item">
 							<Link to="/" className="nav-link">
-								Home <span class="sr-only" />
+								Home
 							</Link>
 						</li>
 						<li className="nav-item">
