f16f736dd5edecebf2eb4a8150a17e19bd0017fb
Osama Adam <osamaadam98@gmail.com>
2019-08-01 16:13:35 +0200

    Properly importing bootstrap and using react-bootstrap for a more responsive navbar.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,42 +1,39 @@
-import React, {useState} from "react";
+import React from "react";
 import {Link} from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 
 function NavBar() {
-	const [navCollapsed, setNavCollapsed] = useState(true);
-
-	const toggleNav = () => {
-		setNavCollapsed(!navCollapsed);
-	};
-
 	return (
 		<div>
-			<nav className="navbar navbar-expand-lg navbar-light bg-light mb-3">
-				<Link to="/" className="navbar-brand">
-					todoList
-				</Link>
-				<button
-					aria-expanded="false"
-					className="navbar-toggle collapsed"
-					onClick={toggleNav}
-					type="button"
-				>
-					<span className="navbar-toggler-icon" />
-				</button>
-				<div className={(navCollapsed ? "collapse" : "") + " navbar-collapse"}>
-					<ul className="navbar-nav">
-						<li className="nav-item">
+			<Navbar
+				collapseOnSelect
+				expand="lg"
+				bg="dark"
+				variant="dark"
+				className="mb-3"
+			>
+				<Navbar.Brand>
+					<Link to="/" className="navbar-brand">
+						todoList
+					</Link>
+				</Navbar.Brand>
+				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+				<Navbar.Collapse id="responsive-navbar-nav">
+					<Nav className="mr-auto">
+						<Nav.Link>
 							<Link to="/" className="nav-link">
 								Home
 							</Link>
-						</li>
-						<li className="nav-item">
+						</Nav.Link>
+						<Nav.Link>
 							<Link to="/about/" className="nav-link">
 								About
 							</Link>
-						</li>
-					</ul>
-				</div>
-			</nav>
+						</Nav.Link>
+					</Nav>
+				</Navbar.Collapse>
+			</Navbar>
 		</div>
 	);
 }
