c58a019b106c280df99734f276493ae4defc5a6c
Osama Adam <osamaadam98@gmail.com>
2019-09-09 02:37:27 +0200

    I'm clueless as to why this works but I couldn't be happeier.

--- server/index.js
+++ server/index.js
@@ -1,26 +1,28 @@
 const express = require("express");
-const todosData = require("./database/todosData.json");
 const fs = require("fs");
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-
 app.get("/api/todosData", (req, res) => {
-	console.log(`received a request from ${req.url}`);
-	res.setHeader("Content-Type", "application/json");
-	res.send(JSON.stringify(todosData));
+	console.log(`received a get request from ${req.url}`);
+	const todosDataRead = fs.createReadStream(
+		__dirname + "/database/todosData.json",
+		"utf8"
+	);
+	todosDataRead.pipe(res);
 });
 
 app.post("/api/todosData", (req, res) => {
-	fs.writeFile(
-		__dirname + "/database/todosData.json",
-		JSON.stringify(req.body),
-		() => res.sendStatus(200)
+	console.log(`received a post request from ${req.url}`);
+	const todosDataWrite = fs.createWriteStream(
+		__dirname + "/database/todosData.json"
 	);
+	req.pipe(todosDataWrite);
+	res.sendStatus(200);
 });
 
-const PORT = process.env.PORT || 3001;
+const port = 4040;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(port, () => {
+	console.log(`Server started on port ${port}`);
+});
